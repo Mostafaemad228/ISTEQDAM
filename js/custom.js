fb5c48b8124946f083a11748b70b3b7601c01685
Mostafa Emad Gomaa <42499279+Mostafaemad228@users.noreply.github.com>
2024-11-18 19:40:33 +0200

    add project files

---
+++ js/custom.js
@@ -0,0 +1,66 @@
+
+  // /////////////////////////////////////////
+  window.addEventListener("scroll" , function () {    
+    var navbar =  document.querySelector("nav");
+    navbar.classList.toggle("sticky",window.scrollY >500);
+  })
+  // ///////////////////////////////////////////////////////////// ///////////////////////
+ /* 1. Proloder */
+$(window).on('load', function () {
+    $('#preloader-active').delay(450).fadeOut('slow');
+    $('body').delay(850).css({
+    'overflow': 'visible'
+    });
+});
+// ////////////////////////// /////////////////////// ///////////////////////
+// var typed = new Typed(".type-text", {
+//   strings: [
+//     "ceo devfolio",
+//     "_بمعنى أن الغاية هي الشكل وليس المحتوى.",
+//     "_بمعنى أن الغاية هي الشكل وليس المحتوى",
+//     "_بمعنى أن الغاية هي الشكل وليس المحتوى",],
+//   typeSpeed: 100,
+//   backSpeed: 50,
+//   // smartBackspace: true,
+//   loop: false,
+//   loopCount: Infinity,
+// });
+
+// ///////////////////////////////////////////////////////////////////////////
+
+  
+  
+
+
+  
+  
+     
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
